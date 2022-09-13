6504b63cc4940144bb7d47835d8e99311b507541
sanaaltassi <47948105+sanaaltassi@users.noreply.github.com>
2022-09-13 16:25:48 +0300

    Add files via upload

---
+++ Models/IllnessesModel.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcApp.Models
+{
+    public class IllnessesModel
+    {
+        [Key]
+        public string Name { get; set; }
+
+        public string IllnessesType { get; set; }
+
+        public int IllnessesId { get; set; }
+        public ICollection<PatientsModel> Patients { get; set; }
+    }
+}
